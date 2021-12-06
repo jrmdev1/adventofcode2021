92b21fdbb69e0594ff28aa7c34174fc6bf820b25
Joseph McFarland <64599252+jrmdev1@users.noreply.github.com>
2021-12-05 20:50:29 -0600

    Add files via upload

--- 3b.py
+++ 3b.py
@@ -5,7 +5,7 @@
 # import re
 import copy
 
-filename = "data3_short.txt"
+filename = "data3.txt"
 
 file = open(filename)
 filestr = file.read()
@@ -31,26 +31,24 @@ print(a_list)
 
 maxcols = len(a_list[0])
 
-ogen = c02 = None
+ogen = None
 current_list = copy.deepcopy(a_list)
 for col in range(maxcols):
     zero_bits = one_bits = 0
-    #col = 0
     for row in current_list:
         if row[col] == "0":
             zero_bits += 1
         elif row[col] == "1":
             one_bits += 1
     if one_bits >= zero_bits:
-        most_common = 1
+        common = 1
     else:
-        most_common = 0
-    #col = 0
+        common = 0
     new_list = []
     for row in current_list:
-        if row[col] == str(most_common):
+        if row[col] == str(common):
             new_list.append(row)
-    print(f"new_list(col={col}):\n{new_list}")
+    #print(f"new_list(col={col}):\n{new_list}")
     if len(new_list) == 1:
         print(f"found entry, {new_list}")
         #found_ogen = new_list[0]
@@ -58,6 +56,36 @@ for col in range(maxcols):
         break
     current_list = copy.deepcopy(new_list)
 print(f"ogen = {ogen}")
+
+co2 = None
+current_list = copy.deepcopy(a_list)
+for col in range(maxcols):
+    zero_bits = one_bits = 0
+    for row in current_list:
+        if row[col] == "0":
+            zero_bits += 1
+        elif row[col] == "1":
+            one_bits += 1
+    if one_bits < zero_bits:    #### TODO: only diff, refactor into one function
+        common = 1      # least common....
+    else:
+        common = 0
+    new_list = []
+    for row in current_list:
+        if row[col] == str(common):
+            new_list.append(row)
+    #print(f"new_list(col={col}):\n{new_list}")
+    if len(new_list) == 1:
+        print(f"found entry, {new_list}")
+        #found_co2 = new_list[0]
+        co2 = int(new_list[0],2)
+        break
+    current_list = copy.deepcopy(new_list)
+print(f"co2 = {co2}")
+
+print(f"mul = {ogen*co2}")
+
+
 
 # print(f"gamma={gamma}, epsilon={epsilon}") 
 # g1 = arraytostr(gamma)
