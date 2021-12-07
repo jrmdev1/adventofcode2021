3cd9fbd1989d9a5f2f579c00dc47c9713f338720
Joseph McFarland <64599252+jrmdev1@users.noreply.github.com>
2021-12-07 12:59:45 -0600

    Add files via upload

--- 4a.py
+++ 4a.py
@@ -19,10 +19,21 @@ draws = [int(elem) for elem in draw_strings]
 print(f"\ndraws={draws}")
 
 #make 2d array (list of lists)
-matrix = []
-for i in range( 2, 2+5 ):     # was 0,maxrows
-    nums = [int(elem) for elem in a_list[i].split()]
-    matrix.append( nums )
-print(f"2d:\n{matrix}")
 
-#print(f"mul = {ogen*co2}")
+
+numboards = int( ((maxrows-2)/6)+1 )
+matrix_list = []
+print(f"numboards = {numboards}, maxrows={maxrows}")
+for boardnum in range( 0, numboards):
+    matrix = []
+    offset = boardnum*6
+    for i in range( 2+offset, 2+offset+5 ):     # was 0,maxrows
+        nums = [int(elem) for elem in a_list[i].split()]
+        matrix.append( nums )
+    print(f"2d:\n{matrix}")
+    matrix_list.append(matrix)
+print(f"matrix_list:\n{matrix_list}")
+
+#print(f"matrix_list[0]:\n {matrix_list[0]}")
+for mat in matrix_list:
+    print(f"mat :\n {mat}")
