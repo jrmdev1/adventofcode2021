5a7973da88e60e8556a47cb4afd11541eb9a66df
Joseph McFarland <64599252+jrmdev1@users.noreply.github.com>
2021-12-07 10:11:47 -0600

    Add files via upload

--- 4a.py
+++ 4a.py
@@ -18,7 +18,11 @@ draw_strings = a_list[0].split(",")
 draws = [int(elem) for elem in draw_strings]
 print(f"\ndraws={draws}")
 
-
-
+#make 2d array (list of lists)
+matrix = []
+for i in range( 2, 2+5 ):     # was 0,maxrows
+    nums = [int(elem) for elem in a_list[i].split()]
+    matrix.append( nums )
+print(f"2d:\n{matrix}")
 
 #print(f"mul = {ogen*co2}")
