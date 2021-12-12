384938868ac062c594ce97f7b3acc1940abee8f1
Joseph McFarland <64599252+jrmdev1@users.noreply.github.com>
2021-12-11 19:05:57 -0600

    Add files via upload

--- 5b.py
+++ 5b.py
@@ -5,12 +5,12 @@
 # import re
 # import copy
 
-max_x = 10
-max_y = 10
-#max_x = 1000
-#max_y = 1000
+#max_x = 10
+#max_y = 10
+max_x = 1000
+max_y = 1000
 grid = [[0]*max_x for _ in range(max_y)] 
-filename = "data5_short.txt"
+filename = "data5.txt"
 
 file = open(filename)
 filestr = file.read()
@@ -30,10 +30,23 @@ def incrRange(xy1, xy2):
             grid[x][y1] += 1
     else:
         print(f"DIAGNOL, {line}")
-        print(f"  {min(x1,x2)}, {max(x1,x2)+1}, {min(y1,y2)}, {max(y1,y2)+1}")
-        for x in range(min(x1,x2), max(x1,x2)+1):
-            for y in range(min(y1,y2), max(y1,y2)+1):
-                grid[x][y] += 1
+        #print(f"  {min(x1,x2)}, {max(x1,x2)+1}, {min(y1,y2)}, {max(y1,y2)+1}")
+        x, y = x1, y1
+        if x1 > x2:
+            xstep = -1
+            len = x1 - x2
+        else:         # 5,5 -> 8,2  x1,y1 -> x2,y2
+            xstep = 1
+            len = x2 - x1
+        if y1 > y2:   # 5,5 -> 8,2  x1,y1 -> x2,y2
+            ystep = -1
+        else:
+            ystep = 1
+        for i in range(len+1):
+            grid[x][y] += 1
+            #print(f"{x},{y}")
+            x += xstep
+            y += ystep
 
 for line in a_list:
     xy = line.split(" -> ")
@@ -44,9 +57,15 @@ for line in a_list:
     print(f"{xy1} : {xy2}")
     incrRange(xy1, xy2)
 
+# dump the grid
+# for y in range(0, max_y):
+#     for x in range(0, max_x):
+#         print(f"{grid[x][y]}",end="")
+#     print("")
+
 count = 0
-for x in range(0, max_x):
-    for y in range(0, max_y):
+for y in range(0, max_y):
+    for x in range(0, max_x):
         if grid[x][y] >= 2:
             count += 1
 print(f"count = {count}")
